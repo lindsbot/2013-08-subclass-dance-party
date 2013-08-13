d6eff256e74e7efa95d92f52de56c88a7e490e3a
Hack Reactor Students <github@hackreactor.com>
2013-08-13 11:55:48 -0700

    fix removeClass bug

--- src/privateDancer.js
+++ src/privateDancer.js
@@ -6,6 +6,5 @@ PrivateDancer.prototype = Object.create(Dancer.prototype);
 PrivateDancer.prototype.constructor = PrivateDancer;
 PrivateDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.$node.removeClass("dancer");
   this.$node.addClass("tina");
 };
