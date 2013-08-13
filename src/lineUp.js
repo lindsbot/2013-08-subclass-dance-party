9beddfc3c744938bdac890ee966e67c09fdc8a8d
Hack Reactor Students <github@hackreactor.com>
2013-08-13 11:00:37 -0700

    add dancer container

---
+++ src/lineUp.js
@@ -0,0 +1,13 @@
+
+//take all elements with the element dancer
+//take all dancers and get the to one side of the dance floor
+$(document).ready(function() {
+  var lineupcont = $('<div class="lineupcontainer"></div>');
+  $('.dancefloor').append(lineupcont);
+});
+
+var lineUp = function() {
+  var dancers = $('.dancer');
+  $.each(dancers, function(index, dancer){
+  });
+};
