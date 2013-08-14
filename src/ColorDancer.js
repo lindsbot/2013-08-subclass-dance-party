62a1eb6adb1f554535e4a5d3691888207e59fce3
Hack Reactor Students <github@hackreactor.com>
2013-08-13 17:00:31 -0700

    add random color method; code cleanup

--- src/ColorDancer.js
+++ src/ColorDancer.js
@@ -3,8 +3,21 @@ var ColorDancer = function(top, left, timeBetweenSteps) {
 };
 
 ColorDancer.prototype = Object.create(Dancer.prototype);
+
 ColorDancer.prototype.constructor = ColorDancer;
+
 ColorDancer.prototype.step = function() {
   Dancer.prototype.step.apply(this);
-  this.$node.toggleClass('blue');
+  var color = this.hexColor();
+  this.$node.css({'border': '10px solid #' + color});
+};
+
+ColorDancer.prototype.hexColor = function(){
+  var hex = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+  var hexNum = '';
+  for (var i = 0; i < 6; i++){
+    hexNum += hex[Math.floor(Math.random() * hex.length)];
+  }
+
+  return hexNum;
 };
